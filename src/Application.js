063137a067c897e57f53da5f0dbd1e803eb773ee
Dandy <dandyarir@gmail.com>
2019-10-11 03:33:30 +0700

    first commit

---
+++ src/Application.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class Application extends Component {
+    render(){
+        let name = 'Dendi'
+        return (
+            <div>
+                <h1>Hello {name}!!!!</h1>
+                <span>KWKWKWKWKWK</span>
+            </div>
+        )
+    }
+}
+
+export default Application;
